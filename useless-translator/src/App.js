992c2fbe5e1d3c1973cb85946c1439156107f3a1
bryanljx <bryanljx1@gmail.com>
2022-01-07 10:41:48 +0800

    Test

--- useless-translator/src/App.js
+++ useless-translator/src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+function fetch_result(translation_string, source_lang, target_lang) {
+  fetch("https://translation.googleapis.com/language/translate/v2?key=AIzaSyCijnhZfoTUvLT9AueRXzHxYURWNXFYjkw", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          // Authorization: `Bearer ${key}`
+        },
+        redirect: "follow",
+        referrer: "no-referrer",
+        body: JSON.stringify({
+          q: translation_string,
+          source: source_lang,
+          target: target_lang,
+          format: "text"
+        })
+      })
+        .then(res => res.json())
+        .then(result => {
+          console.log(result)
+          console.log(result.data.translations[0])
+          })
+        .catch(error => console.log(error)); // reject in case of any error 
+}
 function App() {
+  let translation = fetch_result("Hello World!", 'en', 'es');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Useless Translator</h1>
+      <div className="flex-container">
+        <div className="box">
+          <p>translation box</p>
+        </div>
+        <div>
+          <p>Arrow</p>
+        </div>
+        <div className="box">
+          <p>translated box</p>
+          <p>{translation}</p>
+        </div>
+      </div>
     </div>
   );
 }
